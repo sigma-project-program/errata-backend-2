dd7efed80fa4d34c36dcaea4dc8a5c4bf3cb3753
probro27 <prabhavkhera@hotmail.com>
2022-04-29 17:48:39 +0530

    Fixed the app

---
+++ Desktop/errata-backend-2/Migrations/20220429121339_creation-done.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace errata_backend_2.Migrations
+{
+    public partial class creationdone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
